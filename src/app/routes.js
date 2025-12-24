8f47340be5a295ae7c63ad2f640079b5c579bd68
Daniel Tunku <danieltunku6@gmail.com>
2025-12-24 18:39:49 +0200

    fix: resolve duplicate getMessages declaration

--- src/app/routes.js
+++ src/app/routes.js
@@ -14,7 +14,6 @@ const getUsers = require("./controllers/users/getUsers");
 const getComments = require("./controllers/comments/getComments");
 const getFriendships = require("./controllers/friendships/getFriendships");
 const getMessages = require("./controllers/messages/getMessages");
-const getMessages = require("./controllers/messages/getMessages");
 
 const router = express.Router();
 
@@ -41,6 +40,5 @@ router.get("/healthcheck", healthcheck);
 router.get("/users", getUsers);
 router.get("/comments/:postId", getComments);
 router.get("/friendships", getFriendships);
-router.get("/messages", getMessages);
 router.get("/messages", /*- TODO: auth middleware -*/ getMessages);
 module.exports = router;
