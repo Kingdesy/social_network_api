246656635427942ab0e1b5b1e88e46a775d349c2
kingdesy <desymulombo@gmail.com>
2025-12-24 19:06:21 +0200

    fix fetcheMessage

--- src/app/controllers/messages/getMessages/index.js
+++ src/app/controllers/messages/getMessages/index.js
@@ -1,11 +1,11 @@
 const fetchMessages = require("~root/actions/messages/fetchMessages");
 
 const getMessages = async (req, res) => {
-  const { sender_id, receiver_id } = req.query;
+  const { senderId, receiverId } = req.params;
 
   const { messages } = await fetchMessages({
-    senderId: sender_id ? parseInt(sender_id, 10) : undefined,
-    receiverId: receiver_id ? parseInt(receiver_id, 10) : undefined
+    senderId: senderId ? parseInt(senderId, 10) : undefined,
+    receiverId: receiverId ? parseInt(receiverId, 10) : undefined
   });
 
   return res.send({ messages });
